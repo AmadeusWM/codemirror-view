da9aa1ca7887c223d482dd42a757c8c2f03ba3fe
AmadeusWM <63149896+AmadeusWM@users.noreply.github.com>
2025-08-23 01:44:10 +0200

    Removecode related to control and selection bounds updates in EditContextManager

--- src/domobserver.ts
+++ src/domobserver.ts
@@ -630,10 +630,11 @@ class EditContextManager {
     for (let event in this.handlers) context.addEventListener(event as any, this.handlers[event])
 
     this.measureReq = {read: view => {
-      this.editContext.updateControlBounds(view.contentDOM.getBoundingClientRect())
-      let sel = getSelection(view.root)
-      if (sel && sel.rangeCount)
-        this.editContext.updateSelectionBounds(sel.getRangeAt(0).getBoundingClientRect())
+      // TODO: what was this suppesed to do? messed up my scroll on android tauri???
+      // this.editContext.updateControlBounds(view.contentDOM.getBoundingClientRect())
+      // let sel = getSelection(view.root)
+      // if (sel && sel.rangeCount)
+      //   this.editContext.updateSelectionBounds(sel.getRangeAt(0).getBoundingClientRect())
     }}
   }
 
